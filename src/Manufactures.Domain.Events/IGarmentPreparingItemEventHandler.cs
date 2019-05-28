8d33bbd401c372d29919b8e05bf90b7fd2bb96d9
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2019-05-28 15:03:03 +0700

    Revert "Add Garment Production"
    This reverts commit 8881e7113c3bc29d59bad1e3c9ba4ce6680fee30.

--- src/Manufactures.Domain.Events/IGarmentPreparingItemEventHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿using Infrastructure.Domain.Events;
-namespace Manufactures.Domain.Events
-{
-    public interface IGarmentPreparingItemEventHandler<TEvent> : IDomainEventHandler<TEvent> where TEvent : IGarmentPreparingItemEvent
-    {
-    }
-}
--- src/WebApplication/EntityFrameworkCore/DesignTimeStorageContextFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using ExtCore.Data.EntityFramework;
-
-namespace DanLiris.Admin.Web
-{
-    public class DesignTimeStorageContextFactory : DesignTimeStorageContextFactoryBase<AppStorageContext>
-    {
-    }
-}
