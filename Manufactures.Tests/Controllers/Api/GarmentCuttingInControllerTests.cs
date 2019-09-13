3703f3d2d719eeda92ebc221d7576317859b6bfc
kuswandanu <kuswandanu@gmail.com>
2019-09-13 16:51:33 +0700

    CuttingIn Unit Test

---
+++ Manufactures.Tests/Controllers/Api/GarmentCuttingInControllerTests.cs
@@ -0,0 +1,271 @@
+﻿using Barebone.Tests;
+using Manufactures.Controllers.Api;
+using Manufactures.Domain.GarmentCuttingIns;
+using Manufactures.Domain.GarmentCuttingIns.Commands;
+using Manufactures.Domain.GarmentCuttingIns.ReadModels;
+using Manufactures.Domain.GarmentCuttingIns.Repositories;
+using Manufactures.Domain.GarmentPreparings.ReadModels;
+using Manufactures.Domain.GarmentPreparings.Repositories;
+using Manufactures.Domain.Shared.ValueObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Manufactures.Tests.Controllers.Api
+{
+    public class GarmentCuttingInControllerTests : BaseControllerUnitTest
+    {
+        private Mock<IGarmentCuttingInRepository> _mockGarmentCuttingInRepository;
+        private Mock<IGarmentCuttingInItemRepository> _mockGarmentCuttingInItemRepository;
+        private Mock<IGarmentCuttingInDetailRepository> _mockGarmentCuttingInDetailRepository;
+        private Mock<IGarmentPreparingItemRepository> _mockGarmentPreparingItemRepository;
+
+        public GarmentCuttingInControllerTests() : base()
+        {
+            _mockGarmentCuttingInRepository = CreateMock<IGarmentCuttingInRepository>();
+            _mockGarmentCuttingInItemRepository = CreateMock<IGarmentCuttingInItemRepository>();
+            _mockGarmentCuttingInDetailRepository = CreateMock<IGarmentCuttingInDetailRepository>();
+            _mockGarmentPreparingItemRepository = CreateMock<IGarmentPreparingItemRepository>();
+
+            _MockStorage.SetupStorage(_mockGarmentCuttingInRepository);
+            _MockStorage.SetupStorage(_mockGarmentCuttingInItemRepository);
+            _MockStorage.SetupStorage(_mockGarmentCuttingInDetailRepository);
+            _MockStorage.SetupStorage(_mockGarmentPreparingItemRepository);
+        }
+
+        private GarmentCuttingInController CreateGarmentCuttingInController()
+        {
+            var user = new Mock<ClaimsPrincipal>();
+            var claims = new Claim[]
+            {
+                new Claim("username", "unittestusername")
+            };
+            user.Setup(u => u.Claims).Returns(claims);
+            GarmentCuttingInController controller = (GarmentCuttingInController)Activator.CreateInstance(typeof(GarmentCuttingInController), _MockServiceProvider.Object);
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext()
+                {
+                    User = user.Object
+                }
+            };
+            controller.ControllerContext.HttpContext.Request.Headers["Authorization"] = "Bearer unittesttoken";
+            controller.ControllerContext.HttpContext.Request.Headers["x-timezone-offset"] = "7";
+            controller.ControllerContext.HttpContext.Request.Path = new PathString("/v1/unit-test");
+            return controller;
+        }
+
+        [Fact]
+        public async Task Get_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var unitUnderTest = CreateGarmentCuttingInController();
+
+            _mockGarmentCuttingInRepository
+                .Setup(s => s.Read(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new List<GarmentCuttingInReadModel>().AsQueryable());
+
+            _mockGarmentCuttingInRepository
+                .Setup(s => s.Find(It.IsAny<IQueryable<GarmentCuttingInReadModel>>()))
+                .Returns(new List<GarmentCuttingIn>()
+                {
+                    new GarmentCuttingIn(Guid.NewGuid(), null, null, "RONo", null, new UnitDepartmentId(1), null, null, DateTimeOffset.Now, 0)
+                });
+
+            _mockGarmentCuttingInItemRepository
+                .Setup(s => s.Query)
+                .Returns(new List<GarmentCuttingInItemReadModel>()
+                {
+                    new GarmentCuttingInItemReadModel(Guid.NewGuid())
+                }.AsQueryable());
+
+            _mockGarmentCuttingInDetailRepository
+                .Setup(s => s.Query)
+                .Returns(new List<GarmentCuttingInDetailReadModel>()
+                {
+                    new GarmentCuttingInDetailReadModel(Guid.NewGuid())
+                }.AsQueryable());
+
+            // Act
+            var result = await unitUnderTest.Get();
+
+            // Assert
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(result));
+        }
+
+        [Fact]
+        public async Task GetSingle_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var unitUnderTest = CreateGarmentCuttingInController();
+
+            _mockGarmentCuttingInRepository
+                .Setup(s => s.Find(It.IsAny<Expression<Func<GarmentCuttingInReadModel, bool>>>()))
+                .Returns(new List<GarmentCuttingIn>()
+                {
+                    new GarmentCuttingIn(Guid.NewGuid(), null, null, "RONo", null, new UnitDepartmentId(1), null, null, DateTimeOffset.Now, 0)
+                });
+
+            _mockGarmentCuttingInItemRepository
+                .Setup(s => s.Find(It.IsAny<Expression<Func<GarmentCuttingInItemReadModel, bool>>>()))
+                .Returns(new List<GarmentCuttingInItem>()
+                {
+                    new GarmentCuttingInItem(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), 0, null)
+                });
+
+            _mockGarmentCuttingInDetailRepository
+                .Setup(s => s.Find(It.IsAny<Expression<Func<GarmentCuttingInDetailReadModel, bool>>>()))
+                .Returns(new List<GarmentCuttingInDetail>()
+                {
+                    new GarmentCuttingInDetail(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), new ProductId(1), null, null, null, null, 0, new UomId(1), null, 0, new UomId(1), null, 0, 0)
+                });
+
+            _mockGarmentPreparingItemRepository
+                .Setup(s => s.Query)
+                .Returns(new List<GarmentPreparingItemReadModel>().AsQueryable());
+
+            // Act
+            var result = await unitUnderTest.Get(Guid.NewGuid().ToString());
+
+            // Assert
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(result));
+        }
+
+        [Fact]
+        public async Task Post_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var unitUnderTest = CreateGarmentCuttingInController();
+
+            _MockMediator
+                .Setup(s => s.Send(It.IsAny<PlaceGarmentCuttingInCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new GarmentCuttingIn(Guid.NewGuid(), null, null, "RONo", null, new UnitDepartmentId(1), null, null, DateTimeOffset.Now, 0));
+
+            // Act
+            var result = await unitUnderTest.Post(It.IsAny<PlaceGarmentCuttingInCommand>());
+
+            // Assert
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(result));
+        }
+
+        [Fact]
+        public async Task Put_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var unitUnderTest = CreateGarmentCuttingInController();
+
+            _MockMediator
+                .Setup(s => s.Send(It.IsAny<UpdateGarmentCuttingInCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new GarmentCuttingIn(Guid.NewGuid(), null, null, "RONo", null, new UnitDepartmentId(1), null, null, DateTimeOffset.Now, 0));
+
+            // Act
+            var result = await unitUnderTest.Put(Guid.NewGuid().ToString(), new UpdateGarmentCuttingInCommand());
+
+            // Assert
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(result));
+        }
+
+        [Fact]
+        public async Task Delete_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var unitUnderTest = CreateGarmentCuttingInController();
+
+            _MockMediator
+                .Setup(s => s.Send(It.IsAny<RemoveGarmentCuttingInCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new GarmentCuttingIn(Guid.NewGuid(), null, null, "RONo", null, new UnitDepartmentId(1), null, null, DateTimeOffset.Now, 0));
+
+            // Act
+            var result = await unitUnderTest.Delete(Guid.NewGuid().ToString());
+
+            // Assert
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(result));
+        }
+
+        [Fact]
+        public async Task GetLoaderByRO_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var unitUnderTest = CreateGarmentCuttingInController();
+
+            _mockGarmentCuttingInRepository
+                .Setup(s => s.Read(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new List<GarmentCuttingInReadModel>().AsQueryable());
+
+            _mockGarmentCuttingInRepository
+                .Setup(s => s.Find(It.IsAny<IQueryable<GarmentCuttingInReadModel>>()))
+                .Returns(new List<GarmentCuttingIn>()
+                {
+                    new GarmentCuttingIn(Guid.NewGuid(), null, null, "RONo", null, new UnitDepartmentId(1), null, null, DateTimeOffset.Now, 0)
+                });
+
+            // Act
+            var result = await unitUnderTest.GetLoaderByRO(It.IsAny<string>());
+
+            // Assert
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(result));
+        }
+
+        [Fact]
+        public async Task GetComplete_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var unitUnderTest = CreateGarmentCuttingInController();
+
+            _mockGarmentCuttingInRepository
+                .Setup(s => s.Read(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new List<GarmentCuttingInReadModel>().AsQueryable());
+
+            _mockGarmentCuttingInRepository
+                .Setup(s => s.Find(It.IsAny<IQueryable<GarmentCuttingInReadModel>>()))
+                .Returns(new List<GarmentCuttingIn>()
+                {
+                    new GarmentCuttingIn(Guid.NewGuid(), null, null, "RONo", null, new UnitDepartmentId(1), null, null, DateTimeOffset.Now, 0)
+                });
+
+            _mockGarmentCuttingInItemRepository
+                .Setup(s => s.Query)
+                .Returns(new List<GarmentCuttingInItemReadModel>()
+                {
+                    new GarmentCuttingInItemReadModel(Guid.NewGuid())
+                }.AsQueryable());
+
+            _mockGarmentCuttingInItemRepository
+                .Setup(s => s.Find(It.IsAny<IQueryable<GarmentCuttingInItemReadModel>>()))
+                .Returns(new List<GarmentCuttingInItem>()
+                {
+                    new GarmentCuttingInItem(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), 0, null)
+                });
+
+            _mockGarmentCuttingInDetailRepository
+                .Setup(s => s.Query)
+                .Returns(new List<GarmentCuttingInDetailReadModel>()
+                {
+                    new GarmentCuttingInDetailReadModel(Guid.NewGuid())
+                }.AsQueryable());
+
+            _mockGarmentCuttingInDetailRepository
+                .Setup(s => s.Find(It.IsAny<IQueryable<GarmentCuttingInDetailReadModel>>()))
+                .Returns(new List<GarmentCuttingInDetail>()
+                {
+                    new GarmentCuttingInDetail(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), new ProductId(1), null, null, null, null, 0, new UomId(1), null, 0, new UomId(1), null, 0, 0)
+                });
+
+            // Act
+            var result = await unitUnderTest.GetComplete();
+
+            // Assert
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(result));
+        }
+    }
+}
