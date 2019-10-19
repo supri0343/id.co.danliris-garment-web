dec2ac34422502198c630e9350a3c709c1c0d1a2
achensetyawati <achenzetya@gmail.com>
2019-10-19 09:56:40 +0700

    Update GarmentSewingOutItem.cs

--- src/Manufactures.Domain/GarmentSewingOuts/GarmentSewingOutItem.cs
+++ src/Manufactures.Domain/GarmentSewingOuts/GarmentSewingOutItem.cs
@@ -1,10 +1,91 @@
-﻿using System;
+﻿using Infrastructure.Domain;
+using Manufactures.Domain.Events;
+using Manufactures.Domain.GarmentSewingOuts.ReadModels;
+using Manufactures.Domain.Shared.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Manufactures.Domain.GarmentSewingOuts
 {
-    public class GarmentSewingOutItem
+    public class GarmentSewingOutItem : AggregateRoot<GarmentSewingOutItem, GarmentSewingOutItemReadModel>
     {
+        public Guid SewingOutId { get; private set; }
+        public Guid SewingInId { get; private set; }
+        public Guid SewingInItemId { get; private set; }
+        public ProductId ProductId { get; private set; }
+        public string ProductCode { get; private set; }
+        public string ProductName { get; private set; }
+        public string DesignColor { get; private set; }
+        public SizeId SizeId { get; private set; }
+        public string SizeName { get; private set; }
+        public double Quantity { get; private set; }
+        public UomId UomId { get; private set; }
+        public string UomUnit { get; private set; }
+        public string Color { get; private set; }
+
+        public GarmentSewingOutItem(Guid identity, Guid sewingOutId, Guid sewingInId, Guid sewingInItemId, ProductId productId, string productCode, string productName, string designColor, SizeId sizeId,string sizeName, double quantity, UomId uomId, string uomUnit, string color) : base(identity)
+        {
+            //MarkTransient();
+
+            Identity = identity;
+            SewingOutId = sewingOutId;
+            SewingInId = sewingInId;
+            SewingInItemId = sewingInItemId;
+            ProductId = productId;
+            ProductCode = productCode;
+            ProductName = productName;
+            DesignColor = designColor;
+            SizeId = sizeId;
+            SizeName = sizeName;
+            Quantity = quantity;
+            UomId = uomId;
+            Color = color;
+
+            ReadModel = new GarmentSewingOutItemReadModel(identity)
+            {
+                SewingOutId = SewingOutId,
+                SewingInId = SewingInId,
+                SewingInItemId = SewingInItemId,
+                ProductId = ProductId.Value,
+                ProductCode = ProductCode,
+                ProductName = ProductName,
+                DesignColor = DesignColor,
+                SizeId = SizeId.Value,
+                SizeName = SizeName,
+                Quantity = Quantity,
+                UomId = UomId.Value,
+                Color = Color,
+        };
+
+            ReadModel.AddDomainEvent(new OnGarmentSewingOutPlaced(Identity));
+        }
+
+        public GarmentSewingOutItem(GarmentSewingOutItemReadModel readModel) : base(readModel)
+        {
+            SewingOutId = readModel.SewingOutId;
+            SewingInId = readModel.SewingInId;
+            SewingInItemId = readModel.SewingInItemId;
+            ProductId = new ProductId(readModel.ProductId);
+            ProductCode = readModel.ProductCode;
+            ProductName = readModel.ProductName;
+            DesignColor = readModel.DesignColor;
+            SizeId = new SizeId(readModel.SizeId);
+            SizeName = readModel.SizeName;
+            Quantity = readModel.Quantity;
+            UomId = new UomId( readModel.UomId);
+            Color = readModel.Color;
+        }
+
+        public void Modify()
+        {
+            MarkModified();
+        }
+
+        protected override GarmentSewingOutItem GetEntity()
+        {
+            return this;
+        }
     }
 }
+
