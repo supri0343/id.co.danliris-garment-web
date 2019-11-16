935677f8332b07da4351700ea3bf78d4152adb9b
kuswandanu <kuswandanu@gmail.com>
2019-11-16 12:28:56 +0700

    Read GarmentSewingOut Dynamically

--- src/Manufactures.Data.EntityFrameworkCore/GarmentSewingOuts/Repositories/GarmentSewingOutRepository.cs
+++ src/Manufactures.Data.EntityFrameworkCore/GarmentSewingOuts/Repositories/GarmentSewingOutRepository.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Text;
 
 namespace Manufactures.Data.EntityFrameworkCore.GarmentSewingOuts.Repositories
@@ -39,6 +40,37 @@ namespace Manufactures.Data.EntityFrameworkCore.GarmentSewingOuts.Repositories
             data = data.Skip((page - 1) * size).Take(size);
 
             return data;
+        }
+
+        public IQueryable ReadDynamic(string order, string search, string select, string keyword, string filter)
+        {
+            var data = Query ;
+
+            Dictionary<string, object> FilterDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(filter);
+            data = QueryHelper<GarmentSewingOutReadModel>.Filter(data, FilterDictionary);
+
+            List<string> SearchAttributes = JsonConvert.DeserializeObject<List<string>>(search);
+            if (SearchAttributes.Count == 0)
+            {
+                SearchAttributes = new List<string>
+                {
+                    "SewingOutNo",
+                    "UnitCode",
+                    "UnitToCode",
+                    "RONo",
+                    "Article",
+                    "GarmentSewingOutItem.ProductCode",
+                    "GarmentSewingOutItem.Color"
+                };
+            }
+            data = QueryHelper<GarmentSewingOutReadModel>.Search(data, SearchAttributes, keyword);
+
+            Dictionary<string, string> OrderDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(order);
+            data = OrderDictionary.Count == 0 ? data.OrderByDescending(o => o.ModifiedDate) : QueryHelper<GarmentSewingOutReadModel>.Order(data, OrderDictionary);
+
+            var selectedData = QueryHelper<GarmentSewingOutReadModel>.Select(data, select);
+
+            return selectedData;
         }
 
         protected override GarmentSewingOut Map(GarmentSewingOutReadModel readModel)
