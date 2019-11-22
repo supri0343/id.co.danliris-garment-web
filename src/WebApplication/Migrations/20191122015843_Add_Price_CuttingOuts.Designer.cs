f565ac391c0aba493f14f663f9587d7112f93ae6
achensetyawati <achenzetya@gmail.com>
2019-11-22 09:14:24 +0700

    Update Cutting Out Table

--- src/WebApplication/Migrations/20191122015843_Add_Price_CuttingOuts.Designer.cs
+++ src/WebApplication/Migrations/20191122015843_Add_Price_CuttingOuts.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20191122015843_Add_Price_CuttingOuts")]
+    partial class Add_Price_CuttingOuts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
