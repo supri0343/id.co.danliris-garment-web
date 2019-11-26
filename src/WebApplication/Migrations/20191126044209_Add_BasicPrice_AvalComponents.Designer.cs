60d730d965ea07af05a61f81669e6d4e7a463a77
achensetyawati <achenzetya@gmail.com>
2019-11-26 13:05:50 +0700

    Update Aval Components migration

--- src/WebApplication/Migrations/20191126044209_Add_BasicPrice_AvalComponents.Designer.cs
+++ src/WebApplication/Migrations/20191126044209_Add_BasicPrice_AvalComponents.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20191126044209_Add_BasicPrice_AvalComponents")]
+    partial class Add_BasicPrice_AvalComponents
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
