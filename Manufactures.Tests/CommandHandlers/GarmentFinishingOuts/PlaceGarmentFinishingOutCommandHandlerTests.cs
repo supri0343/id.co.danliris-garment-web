2ef65ccd1768964dd25082a2b574c51c246b3a12
achensetyawati <achenzetya@gmail.com>
2019-12-21 09:57:58 +0700

    Finishing Out Test

---
+++ Manufactures.Tests/CommandHandlers/GarmentFinishingOuts/PlaceGarmentFinishingOutCommandHandlerTests.cs
@@ -0,0 +1,126 @@
+﻿using Barebone.Tests;
+using FluentAssertions;
+using Manufactures.Application.GarmentFinishingOuts.CommandHandlers;
+using Manufactures.Domain.GarmentFinishingIns;
+using Manufactures.Domain.GarmentFinishingIns.ReadModels;
+using Manufactures.Domain.GarmentFinishingIns.Repositories;
+using Manufactures.Domain.GarmentFinishingOuts;
+using Manufactures.Domain.GarmentFinishingOuts.Commands;
+using Manufactures.Domain.GarmentFinishingOuts.ReadModels;
+using Manufactures.Domain.GarmentFinishingOuts.Repositories;
+using Manufactures.Domain.GarmentFinishingOuts.ValueObjects;
+using Manufactures.Domain.Shared.ValueObjects;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Manufactures.Tests.CommandHandlers.GarmentFinishingOuts
+{
+    public class PlaceGarmentFinishingOutCommandHandlerTests : BaseCommandUnitTest
+    {
+        private readonly Mock<IGarmentFinishingOutRepository> _mockFinishingOutRepository;
+        private readonly Mock<IGarmentFinishingOutItemRepository> _mockFinishingOutItemRepository;
+        private readonly Mock<IGarmentFinishingOutDetailRepository> _mockFinishingOutDetailRepository;
+        private readonly Mock<IGarmentFinishingInItemRepository> _mockFinishingInItemRepository;
+
+        public PlaceGarmentFinishingOutCommandHandlerTests()
+        {
+            _mockFinishingOutRepository = CreateMock<IGarmentFinishingOutRepository>();
+            _mockFinishingOutItemRepository = CreateMock<IGarmentFinishingOutItemRepository>();
+            _mockFinishingOutDetailRepository = CreateMock<IGarmentFinishingOutDetailRepository>();
+            _mockFinishingInItemRepository = CreateMock<IGarmentFinishingInItemRepository>();
+
+            _MockStorage.SetupStorage(_mockFinishingOutRepository);
+            _MockStorage.SetupStorage(_mockFinishingOutItemRepository);
+            _MockStorage.SetupStorage(_mockFinishingOutDetailRepository);
+            _MockStorage.SetupStorage(_mockFinishingInItemRepository);
+        }
+        private PlaceGarmentFinishingOutCommandHandler CreatePlaceGarmentFinishingOutCommandHandler()
+        {
+            return new PlaceGarmentFinishingOutCommandHandler(_MockStorage.Object);
+        }
+
+        [Fact]
+        public async Task Handle_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            Guid finishingInItemGuid = Guid.NewGuid();
+            PlaceGarmentFinishingOutCommandHandler unitUnderTest = CreatePlaceGarmentFinishingOutCommandHandler();
+            CancellationToken cancellationToken = CancellationToken.None;
+            PlaceGarmentFinishingOutCommand placeGarmentFinishingOutCommand = new PlaceGarmentFinishingOutCommand()
+            {
+                RONo = "RONo",
+                Unit = new UnitDepartment(1, "UnitCode", "UnitName"),
+                UnitTo = new UnitDepartment(2, "UnitCode2", "UnitName2"),
+                Article = "Article",
+                IsDifferentSize = true,
+                FinishingTo = "FINISHING",
+                Comodity = new GarmentComodity(1, "ComoCode", "ComoName"),
+                FinishingOutDate = DateTimeOffset.Now,
+                Items = new List<GarmentFinishingOutItemValueObject>
+                {
+                    new GarmentFinishingOutItemValueObject
+                    {
+                        Product = new Product(1, "ProductCode", "ProductName"),
+                        Uom = new Uom(1, "UomUnit"),
+                        FinishingInId= new Guid(),
+                        FinishingInItemId=finishingInItemGuid,
+                        Color="Color",
+                        Size=new SizeValueObject(1, "Size"),
+                        IsSave=true,
+                        Quantity=1,
+                        DesignColor= "ColorD",
+                        Details = new List<GarmentFinishingOutDetailValueObject>
+                        {
+                            new GarmentFinishingOutDetailValueObject
+                            {
+                                Size=new SizeValueObject(1, "Size"),
+                                Uom = new Uom(1, "UomUnit"),
+                                Quantity=1
+                            }
+                        }
+                    }
+                },
+
+            };
+
+            _mockFinishingOutRepository
+                .Setup(s => s.Query)
+                .Returns(new List<GarmentFinishingOutReadModel>().AsQueryable());
+            _mockFinishingInItemRepository
+                .Setup(s => s.Query)
+                .Returns(new List<GarmentFinishingInItemReadModel>
+                {
+                    new GarmentFinishingInItemReadModel(finishingInItemGuid)
+                }.AsQueryable());
+
+            _mockFinishingOutRepository
+                .Setup(s => s.Update(It.IsAny<GarmentFinishingOut>()))
+                .Returns(Task.FromResult(It.IsAny<GarmentFinishingOut>()));
+            _mockFinishingOutItemRepository
+                .Setup(s => s.Update(It.IsAny<GarmentFinishingOutItem>()))
+                .Returns(Task.FromResult(It.IsAny<GarmentFinishingOutItem>()));
+            _mockFinishingOutDetailRepository
+                .Setup(s => s.Update(It.IsAny<GarmentFinishingOutDetail>()))
+                .Returns(Task.FromResult(It.IsAny<GarmentFinishingOutDetail>()));
+            _mockFinishingInItemRepository
+                .Setup(s => s.Update(It.IsAny<GarmentFinishingInItem>()))
+                .Returns(Task.FromResult(It.IsAny<GarmentFinishingInItem>()));
+
+            _MockStorage
+                .Setup(x => x.Save())
+                .Verifiable();
+
+            // Act
+            var result = await unitUnderTest.Handle(placeGarmentFinishingOutCommand, cancellationToken);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+    }
+}
