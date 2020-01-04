cf70ef84ccf4ffb4051ecc308484aee0a9a31ea9
achensetyawati <achenzetya@gmail.com>
2020-01-04 11:35:04 +0700

    Add FinishedGoodStocks and FinishedGoodStockHistory

---
+++ src/Manufactures.Data.EntityFrameworkCore/GarmentFinishedGoodStocks/Repositories/GarmentFinishedGoodStockRepository.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Data.EntityFrameworkCore;
+using Manufactures.Domain.GarmentFinishedGoodStocks;
+using Manufactures.Domain.GarmentFinishedGoodStocks.ReadModels;
+using Manufactures.Domain.GarmentFinishedGoodStocks.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Data.EntityFrameworkCore.GarmentFinishedGoodStocks.Repositories
+{
+    public class GarmentFinishedGoodStockRepository : AggregateRepostory<GarmentFinishedGoodStock, GarmentFinishedGoodStockReadModel>, IGarmentFinishedGoodStockRepository
+    {
+        protected override GarmentFinishedGoodStock Map(GarmentFinishedGoodStockReadModel readModel)
+        {
+            return new GarmentFinishedGoodStock(readModel);
+        }
+    }
+}
