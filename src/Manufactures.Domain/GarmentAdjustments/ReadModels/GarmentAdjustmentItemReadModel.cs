9a3ffecc29973760b93d847cfbd66e107df51c3d
achensetyawati <achenzetya@gmail.com>
2020-01-22 11:05:17 +0700

    Add Adjustment

---
+++ src/Manufactures.Domain/GarmentAdjustments/ReadModels/GarmentAdjustmentItemReadModel.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Domain.ReadModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Domain.GarmentAdjustments.ReadModels
+{
+    public class GarmentAdjustmentItemReadModel : ReadModelBase
+    {
+        public GarmentAdjustmentItemReadModel(Guid identity) : base(identity)
+        {
+
+        }
+
+        public Guid AdjustmentId { get; internal set; }
+        public Guid SewingDOItemId { get; internal set; }
+        public int ProductId { get; internal set; }
+        public string ProductCode { get; internal set; }
+        public string ProductName { get; internal set; }
+        public string DesignColor { get; internal set; }
+        public int SizeId { get; internal set; }
+        public string SizeName { get; internal set; }
+        public double Quantity { get; internal set; }
+        public int UomId { get; internal set; }
+        public string UomUnit { get; internal set; }
+        public string Color { get; internal set; }
+        public double BasicPrice { get; internal set; }
+        public double Price { get; internal set; }
+
+        public virtual GarmentAdjustmentReadModel GarmentAdjustment { get; internal set; }
+    }
+}
