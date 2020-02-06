ade554c3c5b7141b9236419faf465ebbd4c21351
sama08 <usamah_alan@hotmail.com>
2020-02-06 08:52:52 +0700

    Update Validation Scrap Source- Scrap Destination

--- src/Manufactures.Domain/GarmentScrapSources/Commands/UpdateGarmentScrapSourceCommand.cs
+++ src/Manufactures.Domain/GarmentScrapSources/Commands/UpdateGarmentScrapSourceCommand.cs
@@ -25,18 +25,18 @@ namespace Manufactures.Domain.GarmentScrapSources.Commands
             public UpdateGarmentScrapSourceCommandValidator(IStorage storage)
             {
                 IGarmentScrapSourceRepository _garmentScrapSourceRepository = storage.GetRepository<IGarmentScrapSourceRepository>();
-                //RuleFor(r => r.Code).Must((c) =>
-                //{
-                //	var a = _garmentScrapSourceRepository.Find(s => s.Code == c);
-                //	return a == null || a.Count < 1;
-                //}).WithMessage("Kode Jenis Barang sudah ada").When(s => s.Code != null);
-                RuleFor(r => r.Code).NotNull().WithMessage("Kode Jenis Barang Aval harus diisi");
-                //RuleFor(r => r.Name).Must((c) =>
-                //{
-                //	var a = _garmentScrapSourceRepository.Find(s => s.Name == c);
-                //	return a == null || a.Count < 1;
-                //}).WithMessage("Nama Jenis Barang sudah ada").When(s => s.Name != null);
-                RuleFor(r => r.Name).NotNull().WithMessage("Nama Jenis Barang Aval harus diisi");
+                RuleFor(r => r.Code).Must((c) =>
+                {
+                    var a = _garmentScrapSourceRepository.Find(s => s.Code == c);
+                    return a == null || a.Count < 1;
+                }).WithMessage("Kode Asal Barang sudah ada").When(s => s.Code != null);
+                RuleFor(r => r.Code).NotNull().WithMessage("Kode Asal Barang Aval harus diisi");
+                RuleFor(r => r.Name).Must((c) =>
+                {
+                    var a = _garmentScrapSourceRepository.Find(s => s.Name == c);
+                    return a == null || a.Count < 1;
+                }).WithMessage("Nama Asal Barang sudah ada").When(s => s.Name != null);
+                RuleFor(r => r.Name).NotNull().WithMessage("Nama Asal Barang Aval harus diisi");
 
             }
         }
