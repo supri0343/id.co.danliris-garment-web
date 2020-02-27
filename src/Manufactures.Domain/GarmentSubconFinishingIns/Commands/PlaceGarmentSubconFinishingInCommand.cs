2ced2869ab30b5106c6478dd88bff94c4a264ebf
kuswandanu <kuswandanu@gmail.com>
2020-02-27 16:26:34 +0700

    Add and Remove Garment Subcon Finishing In

---
+++ src/Manufactures.Domain/GarmentSubconFinishingIns/Commands/PlaceGarmentSubconFinishingInCommand.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation;
+using Infrastructure.Domain.Commands;
+using Manufactures.Domain.GarmentFinishingIns;
+using Manufactures.Domain.GarmentSubconFinishingIns.ValueObjects;
+using Manufactures.Domain.Shared.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Manufactures.Domain.GarmentSubconFinishingIns.Commands
+{
+    public class PlaceGarmentSubconFinishingInCommand : ICommand<GarmentFinishingIn>
+    {
+        public string FinishingInNo { get; set; }
+        public string FinishingInType { get; set; }
+        public UnitDepartment Unit { get; set; }
+        public string Article { get; set; }
+        public string RONo { get; set; }
+        public GarmentComodity Comodity { get; set; }
+        public DateTimeOffset FinishingInDate { get; set; }
+        public List<GarmentSubconFinishingInItemValueObject> Items { get; set; }
+        public long DOId { get; set; }
+        public string DONo { get; set; }
+
+        public object Supplier { get; set; }
+    }
+
+    public class PlaceGarmentSubconFinishingInCommandValidator : AbstractValidator<PlaceGarmentSubconFinishingInCommand>
+    {
+        public PlaceGarmentSubconFinishingInCommandValidator()
+        {
+            RuleFor(r => r.Unit).NotNull();
+            RuleFor(r => r.Unit.Id).NotEmpty().OverridePropertyName("Unit").When(w => w.Unit != null);
+            RuleFor(r => r.FinishingInDate).NotNull().GreaterThan(DateTimeOffset.MinValue).WithMessage("Tanggal FinishingIn Tidak Boleh Kosong");
+            RuleFor(r => r.FinishingInDate).NotNull().LessThan(DateTimeOffset.Now).WithMessage("Tanggal FinishingIn Tidak Boleh Lebih dari Hari Ini");
+            RuleFor(r => r.Comodity).NotNull();
+            RuleFor(r => r.Supplier).NotNull();
+            RuleFor(r => r.DONo).NotNull().When(w => w.Supplier != null);
+            RuleFor(r => r.RONo).NotNull().When(w => w.DONo != null);
+
+            RuleFor(r => r.Items).NotNull().OverridePropertyName("Item");
+            RuleFor(r => r.Items.Where(w => w.IsSave)).NotEmpty().OverridePropertyName("Item").When(w => w.Items != null);
+            RuleForEach(r => r.Items).SetValidator(new GarmentSubconFinishingInItemValueObjectValidator());
+        }
+    }
+
+    class GarmentSubconFinishingInItemValueObjectValidator : AbstractValidator<GarmentSubconFinishingInItemValueObject>
+    {
+        public GarmentSubconFinishingInItemValueObjectValidator()
+        {
+            RuleFor(r => r.Quantity).GreaterThan(0).WithMessage("'Jumlah' harus lebih dari '0'.").When(w => w.IsSave);
+
+            RuleFor(r => r.Product).NotNull().When(w => w.IsSave);
+            RuleFor(r => r.Size).NotNull().When(w => w.IsSave);
+
+        }
+    }
+}
