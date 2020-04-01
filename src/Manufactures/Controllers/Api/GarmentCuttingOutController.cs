67b7d13446d54f800dab3ea682a123cf70ade54b
Lucia Widyana <mo_cia@yahoo.com>
2020-04-01 09:02:19 +0700

    monitoring cutting pembukuan

--- src/Manufactures/Controllers/Api/GarmentCuttingOutController.cs
+++ src/Manufactures/Controllers/Api/GarmentCuttingOutController.cs
@@ -292,12 +292,12 @@ namespace Manufactures.Controllers.Api
 			});
 		}
 		[HttpGet("download")]
-		public async Task<IActionResult> GetXls(int unit, DateTime dateFrom, DateTime dateTo, int page = 1, int size = 25, string Order = "{}")
+		public async Task<IActionResult> GetXls(int unit, DateTime dateFrom, DateTime dateTo, string type,int page = 1, int size = 25, string Order = "{}")
 		{
 			try
 			{
 				VerifyUser();
-				GetXlsCuttingQuery query = new GetXlsCuttingQuery(page, size, Order, unit, dateFrom, dateTo, WorkContext.Token);
+				GetXlsCuttingQuery query = new GetXlsCuttingQuery(page, size, Order, unit, dateFrom, dateTo, type,WorkContext.Token);
 				byte[] xlsInBytes;
 
 				var xls = await Mediator.Send(query);
