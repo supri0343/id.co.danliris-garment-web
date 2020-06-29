60d888ef9ce871fedfed4be6d1a9ab077b33cad3
adepamungkas <ade.pamungkas9@gmail.com>
2020-06-29 16:16:50 +0700

    Create PlaceGarmentDeliveryReturnCommandHandlerTest.cs

---
+++ Manufactures.Tests/CommandHandlers/GarmentDeliveryReturns/CommandHandlers/PlaceGarmentDeliveryReturnCommandHandlerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Tests.CommandHandlers.GarmentDeliveryReturns.CommandHandlers
+{
+    class PlaceGarmentDeliveryReturnCommandHandlerTest
+    {
+    }
+}
