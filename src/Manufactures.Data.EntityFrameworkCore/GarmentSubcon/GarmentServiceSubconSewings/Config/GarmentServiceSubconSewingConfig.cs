d94db41f6c53d59869af933e076e41c2ecdcbc7a
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-01-05 21:38:43 +0700

    Add Migration GarmentServiceSubconSewing

---
+++ src/Manufactures.Data.EntityFrameworkCore/GarmentSubcon/GarmentServiceSubconSewings/Config/GarmentServiceSubconSewingConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Manufactures.Domain.GarmentSubcon.ServiceSubconSewings.ReadModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Manufactures.Data.EntityFrameworkCore.GarmentSubcon.GarmentServiceSubconSewings.Config
+{
+    public class GarmentServiceSubconSewingConfig : IEntityTypeConfiguration<GarmentServiceSubconSewingReadModel>
+    {
+        public void Configure(EntityTypeBuilder<GarmentServiceSubconSewingReadModel> builder)
+        {
+            builder.ToTable("GarmentServiceSubconSewings");
+            builder.HasKey(e => e.Identity);
+
+            builder.Property(a => a.ServiceSubconSewingNo).HasMaxLength(25);
+            builder.Property(a => a.BuyerName).HasMaxLength(100);
+            builder.Property(a => a.BuyerCode).HasMaxLength(25);
+            builder.Property(a => a.RONo).HasMaxLength(25);
+            builder.Property(a => a.Article).HasMaxLength(50);
+            builder.Property(a => a.UnitCode).HasMaxLength(25);
+            builder.Property(a => a.UnitName).HasMaxLength(100);
+            builder.Property(a => a.ComodityCode).HasMaxLength(25);
+            builder.Property(a => a.ComodityName).HasMaxLength(100);
+
+            builder.HasIndex(i => i.ServiceSubconSewingNo).IsUnique().HasFilter("[Deleted]=(0)");
+            builder.ApplyAuditTrail();
+            builder.ApplySoftDelete();
+        }
+    }
+}
---
+++ src/Manufactures.Data.EntityFrameworkCore/GarmentSubcon/GarmentServiceSubconSewings/Config/GarmentServiceSubconSewingItemConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Manufactures.Domain.GarmentSubcon.ServiceSubconSewings.ReadModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Manufactures.Data.EntityFrameworkCore.GarmentSubcon.GarmentServiceSubconSewings.Config
+{
+    public class GarmentServiceSubconSewingItemConfig : IEntityTypeConfiguration<GarmentServiceSubconSewingItemReadModel>
+    {
+        public void Configure(EntityTypeBuilder<GarmentServiceSubconSewingItemReadModel> builder)
+        {
+            builder.ToTable("GarmentServiceSubconSewingItems");
+            builder.HasKey(e => e.Identity);
+            builder.HasOne(a => a.GarmentServiceSubconSewingIdentity)
+                   .WithMany(a => a.GarmentServiceSubconSewingItem)
+                   .HasForeignKey(a => a.ServiceSubconSewingId);
+
+            builder.Property(a => a.ProductCode)
+               .HasMaxLength(25);
+            builder.Property(a => a.ProductName)
+               .HasMaxLength(100);
+            builder.Property(a => a.DesignColor)
+               .HasMaxLength(2000);
+
+            builder.Property(a => a.SizeName)
+               .HasMaxLength(100);
+
+            builder.Property(a => a.UomUnit)
+               .HasMaxLength(25);
+
+            builder.ApplyAuditTrail();
+            builder.ApplySoftDelete();
+        }
+    }
+}
