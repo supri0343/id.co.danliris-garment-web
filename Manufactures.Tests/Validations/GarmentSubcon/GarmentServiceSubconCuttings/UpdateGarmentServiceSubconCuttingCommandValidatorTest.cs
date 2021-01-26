da361c28dd3feb3459b43c3813002951e490dac5
achensetyawati <setyawati@moonlay.com>
2021-01-26 09:06:03 +0700

    update tests

---
+++ Manufactures.Tests/Validations/GarmentSubcon/GarmentServiceSubconCuttings/UpdateGarmentServiceSubconCuttingCommandValidatorTest.cs
@@ -0,0 +1,88 @@
+﻿using FluentValidation.TestHelper;
+using Manufactures.Domain.GarmentSubcon.ServiceSubconCuttings.Commands;
+using Manufactures.Domain.GarmentSubcon.ServiceSubconCuttings.ValueObjects;
+using Manufactures.Domain.Shared.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Manufactures.Tests.Validations.GarmentSubcon.GarmentServiceSubconCuttings
+{
+    public class UpdateGarmentServiceSubconCuttingCommandValidatorTest
+    {
+        private UpdateGarmentServiceSubconCuttingCommandValidator GetValidationRules()
+        {
+            return new UpdateGarmentServiceSubconCuttingCommandValidator();
+        }
+
+        [Fact]
+        public void Place_ShouldHaveError()
+        {
+            // Arrange
+
+            var unitUnderTest = new UpdateGarmentServiceSubconCuttingCommand();
+
+            // Action
+            var validator = GetValidationRules();
+            var result = validator.TestValidate(unitUnderTest);
+
+            // Assert
+            result.ShouldHaveError();
+        }
+
+        [Fact]
+        public void Place_ShouldNotHaveError()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            var unitUnderTest = new UpdateGarmentServiceSubconCuttingCommand()
+            {
+                IsUsed = true,
+                Article = "Article",
+                RONo = "RONo",
+                Comodity = new GarmentComodity()
+                {
+                    Id = 1,
+                    Code = "Code",
+                    Name = "Name"
+                },
+                SubconDate = DateTimeOffset.Now,
+                SubconNo = "CuttingOutNo",
+                Unit = new UnitDepartment()
+                {
+                    Id = 1,
+                    Code = "Code",
+                    Name = "Name"
+                },
+                Items = new List<GarmentServiceSubconCuttingItemValueObject>()
+                {
+                    new GarmentServiceSubconCuttingItemValueObject()
+                    {
+                        DesignColor ="DesignColor",
+                        Id =id,
+                        IsSave =true,
+                        Product =new Product()
+                        {
+                            Id = 1,
+                            Code = "Code",
+                            Name = "Name"
+                        },
+                        Quantity =1,
+                        CuttingInDetailId =id,
+                        CuttingInQuantity =1,
+                        ServiceSubconCuttingId =id,
+
+                    }
+                }
+            };
+            // Action
+            var validator = GetValidationRules();
+            var result = validator.TestValidate(unitUnderTest);
+
+            // Assert
+            result.ShouldNotHaveError();
+
+        }
+    }
+}
---
+++ Manufactures.Tests/Validations/GarmentSubcon/GarmentSubconContracts/PlaceGarmentSubconContractValidationTest.cs
@@ -0,0 +1,67 @@
+﻿using FluentValidation.TestHelper;
+using Manufactures.Domain.GarmentSubcon.SubconContracts.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Manufactures.Domain.Shared.ValueObjects;
+
+namespace Manufactures.Tests.Validations.GarmentSubcon.GarmentSubconContracts
+{
+    public class PlaceGarmentSubconContractValidationTest
+    {
+        private PlaceGarmentSubconContractCommandValidator GetValidationRules()
+        {
+            return new PlaceGarmentSubconContractCommandValidator();
+        }
+
+        [Fact]
+        public void Place_ShouldHaveError()
+        {
+            // Arrange
+
+            var unitUnderTest = new PlaceGarmentSubconContractCommand();
+
+            // Action
+            var validator = GetValidationRules();
+            var result = validator.TestValidate(unitUnderTest);
+
+            // Assert
+            result.ShouldHaveError();
+        }
+
+        [Fact]
+        public void Place_ShouldNotHaveError()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            var unitUnderTest = new PlaceGarmentSubconContractCommand()
+            {
+                AgreementNo = "test",
+                BPJNo = "test",
+                ContractNo = "test",
+                ContractType = "test",
+                DueDate = DateTimeOffset.Now,
+                FinishedGoodType = "test",
+                JobType = "test",
+                Quantity = 1,
+                ContractDate = DateTimeOffset.Now,
+                Supplier = new Supplier
+                {
+                    Code = "test",
+                    Id = 1,
+                    Name = "test"
+                },
+                IsUsed=false
+            };
+            // Action
+            var validator = GetValidationRules();
+            var result = validator.TestValidate(unitUnderTest);
+
+            // Assert
+            result.ShouldNotHaveError();
+
+        }
+
+    }
+}
