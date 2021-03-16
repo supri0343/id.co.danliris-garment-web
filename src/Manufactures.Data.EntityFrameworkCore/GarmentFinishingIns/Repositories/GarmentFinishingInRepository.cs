d424b4278da87f31a937619f27135c472c00643f
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-03-16 10:41:53 +0700

    Update Time Load RO
    Finishing Out, Cutting In, Cutting Out, Expenditure Good, Expenditure Good Return, Sewing Out

--- src/Manufactures.Data.EntityFrameworkCore/GarmentFinishingIns/Repositories/GarmentFinishingInRepository.cs
+++ src/Manufactures.Data.EntityFrameworkCore/GarmentFinishingIns/Repositories/GarmentFinishingInRepository.cs
@@ -42,6 +42,27 @@ namespace Manufactures.Data.EntityFrameworkCore.GarmentFinishingIns.Repositories
             return data;
         }
 
+        public IQueryable<GarmentFinishingInReadModel> ReadComplete(int page, int size, string order, string keyword, string filter)
+        {
+            var data = Query;
+
+            Dictionary<string, object> FilterDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(filter);
+            data = QueryHelper<GarmentFinishingInReadModel>.Filter(data, FilterDictionary);
+
+            List<string> SearchAttributes = new List<string>
+            {
+                "RONo",
+            };
+            data = QueryHelper<GarmentFinishingInReadModel>.Search(data, SearchAttributes, keyword);
+
+            Dictionary<string, string> OrderDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(order);
+            data = OrderDictionary.Count == 0 ? data.OrderByDescending(o => o.ModifiedDate) : QueryHelper<GarmentFinishingInReadModel>.Order(data, OrderDictionary);
+
+            //data = data.Skip((page - 1) * size).Take(size);
+
+            return data;
+        }
+
         protected override GarmentFinishingIn Map(GarmentFinishingInReadModel readModel)
         {
             return new GarmentFinishingIn(readModel);
