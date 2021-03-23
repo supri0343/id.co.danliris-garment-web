34f38beecd1e180519fd24d6b7e6e16cd709a4f0
achensetyawati <setyawati@moonlay.com>
2021-03-23 14:00:23 +0700

    update Loading
    Generate Sewing In (CUTTING)

--- src/Manufactures.Application/GarmentLoadings/CommandHandlers/RemoveGarmentLoadingCommandHandler.cs
+++ src/Manufactures.Application/GarmentLoadings/CommandHandlers/RemoveGarmentLoadingCommandHandler.cs
@@ -5,6 +5,8 @@ using Manufactures.Domain.GarmentLoadings.Commands;
 using Manufactures.Domain.GarmentLoadings.Repositories;
 using Manufactures.Domain.GarmentSewingDOs;
 using Manufactures.Domain.GarmentSewingDOs.Repositories;
+using Manufactures.Domain.GarmentSewingIns;
+using Manufactures.Domain.GarmentSewingIns.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +22,8 @@ namespace Manufactures.Application.GarmentLoadings.CommandHandlers
         private readonly IGarmentLoadingRepository _garmentLoadingRepository;
         private readonly IGarmentLoadingItemRepository _garmentLoadingItemRepository;
         private readonly IGarmentSewingDOItemRepository _garmentSewingDOItemRepository;
+        private readonly IGarmentSewingInRepository _garmentSewingInRepository;
+        private readonly IGarmentSewingInItemRepository _garmentSewingInItemRepository;
 
         public RemoveGarmentLoadingCommandHandler(IStorage storage)
         {
@@ -27,11 +31,14 @@ namespace Manufactures.Application.GarmentLoadings.CommandHandlers
             _garmentLoadingRepository = storage.GetRepository<IGarmentLoadingRepository>();
             _garmentLoadingItemRepository = storage.GetRepository<IGarmentLoadingItemRepository>();
             _garmentSewingDOItemRepository = storage.GetRepository<IGarmentSewingDOItemRepository>();
+            _garmentSewingInRepository = storage.GetRepository<IGarmentSewingInRepository>();
+            _garmentSewingInItemRepository = storage.GetRepository<IGarmentSewingInItemRepository>();
         }
 
         public async Task<GarmentLoading> Handle(RemoveGarmentLoadingCommand request, CancellationToken cancellationToken)
         {
             var loading = _garmentLoadingRepository.Query.Where(o => o.Identity == request.Identity).Select(o => new GarmentLoading(o)).Single();
+            var garmentSewingIn = _garmentSewingInRepository.Query.Where(o => o.LoadingId == request.Identity).Select(o => new GarmentSewingIn(o)).Single();
 
             Dictionary<Guid, double> sewingDOItemToBeUpdated = new Dictionary<Guid, double>();
 
@@ -60,8 +67,18 @@ namespace Manufactures.Application.GarmentLoadings.CommandHandlers
                 await _garmentSewingDOItemRepository.Update(garmentSewingDOItem);
             }
 
+            var garmentSewingInItems = _garmentSewingInItemRepository.Find(x => x.SewingInId == garmentSewingIn.Identity);
+
+            foreach (var item in garmentSewingInItems)
+            {
+                item.Remove();
+                await _garmentSewingInItemRepository.Update(item);
+            }
+
             loading.Remove();
+            garmentSewingIn.Remove();
             await _garmentLoadingRepository.Update(loading);
+            await _garmentSewingInRepository.Update(garmentSewingIn);
 
             _storage.Save();
 
