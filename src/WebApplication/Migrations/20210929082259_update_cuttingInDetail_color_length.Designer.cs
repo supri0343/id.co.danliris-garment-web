760b045eb9908ed1ba613c5787e442cafee1530c
achensetyawati <setyawati@moonlay.com>
2021-09-29 15:28:26 +0700

    update cutting in column color

--- src/WebApplication/Migrations/20210929082259_update_cuttingInDetail_color_length.Designer.cs
+++ src/WebApplication/Migrations/20210929082259_update_cuttingInDetail_color_length.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20210929082259_update_cuttingInDetail_color_length")]
+    partial class update_cuttingInDetail_color_length
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
