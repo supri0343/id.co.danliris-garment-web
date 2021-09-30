821850146cb8cb3dc2983f423352b9c2d6ef98c1
achensetyawati <setyawati@moonlay.com>
2021-09-30 15:55:39 +0700

    update color max length adjustment, avalcomponents

--- src/WebApplication/Migrations/20210930084109_update_adjustments_avalComponents_color_length.Designer.cs
+++ src/WebApplication/Migrations/20210930084109_update_adjustments_avalComponents_color_length.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20210930084109_update_adjustments_avalComponents_color_length")]
+    partial class update_adjustments_avalComponents_color_length
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
