3e2d4632e191259a8aac7aa29c2dbee09ed72b78
Ady Hermawan <ady.hermawan@moonlay.com>
2021-10-28 10:05:55 +0700

    Add Feature Garmet Subcon Fabric Wash

---
+++ src/Manufactures.Domain/GarmentSubcon/ServiceSubconFabricWashes/Commands/PlaceGarmentServiceSubconFabricWashCommand.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Domain.Commands;
+using Manufactures.Domain.GarmentSubcon.ServiceSubconFabricWashes.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Domain.GarmentSubcon.ServiceSubconFabricWashes.Commands
+{
+    public class PlaceGarmentServiceSubconFabricWashCommand : ICommand<GarmentServiceSubconFabricWash>
+    {
+        public string ServiceSubconFabricWashNo { get; set; }
+        public DateTimeOffset? ServiceSubconFabricWashDate { get; set; }
+        public bool IsUsed { get; set; }
+        public List<GarmentServiceSubconFabricWashItemValueObject> Items { get; set; }
+        public bool IsSave { get; set; }
+    }
+}
---
+++ src/Manufactures/Dtos/GarmentSubcon/GarmentServiceSubconFabricWashes/GarmentServiceSubconFabricWashDto.cs
@@ -0,0 +1,26 @@
+﻿using Manufactures.Domain.GarmentSubcon.ServiceSubconFabricWashes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Dtos.GarmentSubcon.GarmentServiceSubconFabricWashes
+{
+    public class GarmentServiceSubconFabricWashDto
+    {
+        public GarmentServiceSubconFabricWashDto(GarmentServiceSubconFabricWash garmentServiceSubconFabricWashList)
+        {
+            Id = garmentServiceSubconFabricWashList.Identity;
+            ServiceSubconFabricWashNo = garmentServiceSubconFabricWashList.ServiceSubconFabricWashNo;
+            ServiceSubconFabricWashDate = garmentServiceSubconFabricWashList.ServiceSubconFabricWashDate;
+            IsUsed = garmentServiceSubconFabricWashList.IsUsed;
+            Items = new List<GarmentServiceSubconFabricWashItemDto>();
+        }
+
+        public Guid Id { get; internal set; }
+        public string ServiceSubconFabricWashNo { get; set; }
+        public DateTimeOffset ServiceSubconFabricWashDate { get; set; }
+        public bool IsUsed { get; set; }
+
+        public virtual List<GarmentServiceSubconFabricWashItemDto> Items { get; internal set; }
+    }
+}
