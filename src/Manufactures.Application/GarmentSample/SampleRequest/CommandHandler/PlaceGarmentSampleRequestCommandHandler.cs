7eab89e1e49a51adc0b4da179be7422f8e16467c
achensetyawati <setyawati@moonlay.com>
2021-12-10 09:19:13 +0700

    Update PlaceGarmentSampleRequestCommandHandler.cs

--- src/Manufactures.Application/GarmentSample/SampleRequest/CommandHandler/PlaceGarmentSampleRequestCommandHandler.cs
+++ src/Manufactures.Application/GarmentSample/SampleRequest/CommandHandler/PlaceGarmentSampleRequestCommandHandler.cs
@@ -76,6 +76,7 @@ namespace Manufactures.Application.GarmentSample.SampleRequest.CommandHandler
 
             foreach (var specification in request.SampleSpecifications)
             {
+
                 GarmentSampleRequestSpecification GarmentSampleRequestSpecification = new GarmentSampleRequestSpecification(
                     Guid.NewGuid(),
                     GarmentSampleRequest.Identity,
