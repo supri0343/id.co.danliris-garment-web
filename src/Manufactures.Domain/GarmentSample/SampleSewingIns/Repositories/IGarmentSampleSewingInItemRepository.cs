b303cb85689b620c5f5f80e5a9fc4ebf759dc256
achensetyawati <setyawati@moonlay.com>
2021-12-21 08:45:15 +0700

    Garment Sample Cutting Out Service

--- src/Manufactures.Domain/GarmentSample/SampleSewingIns/Repositories/IGarmentSampleSewingInItemRepository.cs
+++ src/Manufactures.Domain/GarmentSample/SampleSewingIns/Repositories/IGarmentSampleSewingInItemRepository.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Infrastructure.Domain.Repositories;
+using Manufactures.Domain.GarmentSample.SampleSewingIns.ReadModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Manufactures.Domain.GarmentSample.SampleSewingIns.Repositories
 {
-    public interface IGarmentSampleSewingInItemRepository
+    public interface IGarmentSampleSewingInItemRepository : IAggregateRepository<GarmentSampleSewingInItem, GarmentSampleSewingInItemReadModel>
     {
     }
 }
--- src/Manufactures.Domain/GarmentSample/SampleSewingIns/Repositories/IGarmentSampleSewingInRepository.cs
+++ src/Manufactures.Domain/GarmentSample/SampleSewingIns/Repositories/IGarmentSampleSewingInRepository.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Infrastructure.Domain.Repositories;
+using Manufactures.Domain.GarmentSample.SampleSewingIns.ReadModels;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Manufactures.Domain.GarmentSample.SampleSewingIns.Repositories
 {
-    public interface IGarmentSampleSewingInRepository
+    public interface IGarmentSampleSewingInRepository : IAggregateRepository<GarmentSampleSewingIn, GarmentSampleSewingInReadModel>
     {
+        IQueryable<GarmentSampleSewingInReadModel> Read(int page, int size, string order, string keyword, string filter);
+        IQueryable<GarmentSampleSewingInReadModel> ReadComplete(int page, int size, string order, string keyword, string filter);
     }
+
 }
