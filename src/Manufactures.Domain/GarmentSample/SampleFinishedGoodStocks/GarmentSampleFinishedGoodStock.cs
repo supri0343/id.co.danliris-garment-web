618ed01ade4deeca67e538841f71d4ccb0d2ba0d
achensetyawati <setyawati@moonlay.com>
2022-01-11 10:50:02 +0700

    G-Sample FinishingOuts

---
+++ src/Manufactures.Domain/GarmentSample/SampleFinishedGoodStocks/GarmentSampleFinishedGoodStock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Domain.GarmentSample.SampleFinishedGoodStocks
+{
+    class GarmentSampleFinishedGoodStock
+    {
+    }
+}
--- src/WebApplication/Migrations/20220111034905_Initial_SampleFinishingOuts.Designer.cs
+++ src/WebApplication/Migrations/20220111034905_Initial_SampleFinishingOuts.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20220111034905_Initial_SampleFinishingOuts")]
+    partial class Initial_SampleFinishingOuts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
