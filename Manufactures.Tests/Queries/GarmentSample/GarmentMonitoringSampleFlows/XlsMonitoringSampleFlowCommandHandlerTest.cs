191c121e01a6be929c32da6bcf615c89f0cc937e
mocia <mo_cia@yahoo.com>
2022-01-27 10:11:12 +0700

    Update XlsMonitoringSampleFlowCommandHandlerTest.cs

--- Manufactures.Tests/Queries/GarmentSample/GarmentMonitoringSampleFlows/XlsMonitoringSampleFlowCommandHandlerTest.cs
+++ Manufactures.Tests/Queries/GarmentSample/GarmentMonitoringSampleFlows/XlsMonitoringSampleFlowCommandHandlerTest.cs
@@ -8,6 +8,7 @@ using Manufactures.Domain.GarmentSample.SampleCuttingOuts.Repositories;
 using Manufactures.Domain.GarmentSample.SampleFinishingOuts;
 using Manufactures.Domain.GarmentSample.SampleFinishingOuts.ReadModels;
 using Manufactures.Domain.GarmentSample.SampleFinishingOuts.Repositories;
+using Manufactures.Domain.GarmentSample.SampleRequests.Repositories;
 using Manufactures.Domain.GarmentSample.SampleSewingOuts;
 using Manufactures.Domain.GarmentSample.SampleSewingOuts.ReadModels;
 using Manufactures.Domain.GarmentSample.SampleSewingOuts.Repositories;
@@ -39,6 +40,8 @@ namespace Manufactures.Tests.GarmentSample.Queries.GarmentMonitoringProductionFl
 		private readonly Mock<IGarmentSampleFinishingOutRepository> _mockGarmentFinishingOutRepository;
 		private readonly Mock<IGarmentSampleFinishingOutItemRepository> _mockGarmentFinishingOutItemRepository;
 		private readonly Mock<IGarmentSampleFinishingOutDetailRepository> _mockGarmentFinishingOutDetailRepository;
+		private readonly Mock<IGarmentSampleRequestRepository> _mockGarmentSampleRequestRepository;
+		private readonly Mock<IGarmentSampleRequestProductRepository> _mockGarmentSampleRequestProductRepository;
 		protected readonly Mock<IHttpClientService> _mockhttpService;
 		private Mock<IServiceProvider> serviceProviderMock;
 
@@ -64,6 +67,11 @@ namespace Manufactures.Tests.GarmentSample.Queries.GarmentMonitoringProductionFl
 			_MockStorage.SetupStorage(_mockGarmentCuttingOutRepository);
 			_MockStorage.SetupStorage(_mockGarmentCuttingOutItemRepository);
 			_MockStorage.SetupStorage(_mockGarmentCuttingOutDetailRepository);
+
+			_mockGarmentSampleRequestRepository = CreateMock<IGarmentSampleRequestRepository>();
+			_mockGarmentSampleRequestProductRepository = CreateMock<IGarmentSampleRequestProductRepository>();
+			_MockStorage.SetupStorage(_mockGarmentSampleRequestRepository);
+			_MockStorage.SetupStorage(_mockGarmentSampleRequestProductRepository);
 
 			serviceProviderMock = new Mock<IServiceProvider>();
 			_mockhttpService = CreateMock<IHttpClientService>();
@@ -173,6 +181,20 @@ namespace Manufactures.Tests.GarmentSample.Queries.GarmentMonitoringProductionFl
 				.Returns(new List<GarmentSampleSewingOutReadModel>
 				{
 					new GarmentSampleSewingOut(guidSewingOut,"",new BuyerId(1),"","",new UnitDepartmentId(1),"","","FINISHING",DateTimeOffset.Now,"ro","",new UnitDepartmentId(1),"","",new GarmentComodityId(1),"","",true).GetReadModel()
+				}.AsQueryable());
+
+			_mockGarmentSampleRequestProductRepository
+			  .Setup(s => s.Query)
+			  .Returns(new List<GarmentSampleRequestProductReadModel>
+			  {
+					new Domain.GarmentSample.SampleRequests.GarmentSampleRequestProduct (guidSampleReqId,guidSampleReqId,"","",new Domain.Shared.ValueObjects.SizeId(1),"","",10,1).GetReadModel()
+			  }.AsQueryable());
+
+			_mockGarmentSampleRequestRepository
+				.Setup(s => s.Query)
+				.Returns(new List<GarmentSampleRequestReadModel>
+				{
+					new Domain.GarmentSample.SampleRequests.GarmentSampleRequest (guidSampleReqId,"","","ro","ro",DateTimeOffset.Now,new Domain.Shared.ValueObjects.BuyerId(1),"","",new Domain.Shared.ValueObjects.GarmentComodityId(1),"","","","",DateTimeOffset.Now,"","","",true,true,DateTimeOffset.Now,"",false,DateTimeOffset.Now,"","",false,DateTimeOffset.Now,"","","","","","",new Domain.Shared.ValueObjects.SectionId(1),"").GetReadModel()
 				}.AsQueryable());
 			// Act
 			var result = await unitUnderTest.Handle(getMonitoring, cancellationToken);
