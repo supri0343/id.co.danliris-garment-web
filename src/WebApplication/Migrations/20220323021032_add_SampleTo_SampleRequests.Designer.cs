494182b48625c2878d04262b3f9ce8cf8c430a9e
achensetyawati <setyawati@moonlay.com>
2022-03-25 07:45:15 +0700

    add_SampleTo (Tipe Sample) Sample Request

--- src/WebApplication/Migrations/20220323021032_add_SampleTo_SampleRequests.Designer.cs
+++ src/WebApplication/Migrations/20220323021032_add_SampleTo_SampleRequests.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20220323021032_add_SampleTo_SampleRequests")]
+    partial class add_SampleTo_SampleRequests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
