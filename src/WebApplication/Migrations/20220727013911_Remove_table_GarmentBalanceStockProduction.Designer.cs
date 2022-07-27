a7020b21cb15ac06fe8c8c3b0ae68060dffeb0ff
ardana96 <ardanagama@gmail.com>
2022-07-27 08:54:59 +0700

    Remoe TableGarmentBalanceStockProduction

--- src/WebApplication/Migrations/20220727013911_Remove_table_GarmentBalanceStockProduction.Designer.cs
+++ src/WebApplication/Migrations/20220727013911_Remove_table_GarmentBalanceStockProduction.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20220727013911_Remove_table_GarmentBalanceStockProduction")]
+    partial class Remove_table_GarmentBalanceStockProduction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
