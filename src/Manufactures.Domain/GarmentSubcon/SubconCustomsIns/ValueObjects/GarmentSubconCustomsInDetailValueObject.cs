b049de0de06b17ef04df260127d43a3b5f916d6a
achensetyawati <achenzetya@gmail.com>
2022-08-26 11:10:26 +0700

    update Subcon Customs In

---
+++ src/Manufactures.Domain/GarmentSubcon/SubconCustomsIns/ValueObjects/GarmentSubconCustomsInDetailValueObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Domain.GarmentSubcon.SubconCustomsIns.ValueObjects
+{
+    class GarmentSubconCustomsInDetailValueObject
+    {
+    }
+}
