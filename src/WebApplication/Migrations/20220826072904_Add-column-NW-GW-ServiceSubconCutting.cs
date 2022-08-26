5a1842b69c95630d629f1ace77d09b71405e48f3
Joifarflex <joifarjosuajosua@gmail.com>
2022-08-26 15:46:51 +0700

    update subcon service cutting
    add column nw (nett weight) and gw (gross weight) on header

---
+++ src/WebApplication/Migrations/20220826072904_Add-column-NW-GW-ServiceSubconCutting.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DanLiris.Admin.Web.Migrations
+{
+    public partial class AddcolumnNWGWServiceSubconCutting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "GrossWeight",
+                table: "GarmentServiceSubconCuttings",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "NettWeight",
+                table: "GarmentServiceSubconCuttings",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GrossWeight",
+                table: "GarmentServiceSubconCuttings");
+
+            migrationBuilder.DropColumn(
+                name: "NettWeight",
+                table: "GarmentServiceSubconCuttings");
+        }
+    }
+}
