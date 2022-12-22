a954a093fea5fa5a3c1bc5e8c44e4dbe06464053
supri0343 <63761987+supri0343@users.noreply.github.com>
2022-12-22 18:01:58 +0700

    enhance-GetPreparing-With-BC

--- src/Manufactures.Application/GarmentAvalProducts/Queries/GetForLoaderAval-BC/GetForLoaderAval-BCQuery.cs
+++ src/Manufactures.Application/GarmentAvalProducts/Queries/GetForLoaderAval-BC/GetForLoaderAval-BCQuery.cs
@@ -8,14 +8,14 @@ namespace Manufactures.Application.GarmentAvalProducts.Queries.GetForLoaderAval_
     public class GetForLoaderAval_BCQuery : IQuery<GetForLoaderAval_BCViewModel>
     {
         //public int page { get; private set; }
-        public int unit { get; private set; }
+        public int? unit { get; private set; }
         public string ro { get; private set; }
         //public bool filter { get; private set; }
         //public string keyword { get; private set; }
         public string token { get; private set; }
 
 
-        public GetForLoaderAval_BCQuery(int unit, string ro, string token)
+        public GetForLoaderAval_BCQuery(int? unit, string ro, string token)
         {
             //this.page = page;
             //this.size = size;
