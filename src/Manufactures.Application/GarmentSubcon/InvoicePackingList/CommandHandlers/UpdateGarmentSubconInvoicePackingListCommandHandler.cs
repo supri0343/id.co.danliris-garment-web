053b5921827553e3e474039575835c5c364e4198
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-06-14 19:00:05 +0700

    enhance-SubconInvoicePackingList

--- src/Manufactures.Application/GarmentSubcon/InvoicePackingList/CommandHandlers/UpdateGarmentSubconInvoicePackingListCommandHandler.cs
+++ src/Manufactures.Application/GarmentSubcon/InvoicePackingList/CommandHandlers/UpdateGarmentSubconInvoicePackingListCommandHandler.cs
@@ -2,7 +2,9 @@
 using Infrastructure.Domain.Commands;
 using Manufactures.Domain.GarmentSubcon.InvoicePackingList;
 using Manufactures.Domain.GarmentSubcon.InvoicePackingList.Commands;
+using Manufactures.Domain.GarmentSubcon.InvoicePackingList.ISubconInvoicePackingListReceiptItemRepositories;
 using Manufactures.Domain.GarmentSubcon.InvoicePackingList.Repositories;
+using Manufactures.Domain.GarmentSubcon.SubconInvoicePackingListReceiptItemModel;
 using Manufactures.Domain.Shared.ValueObjects;
 using System;
 using System.Collections.Generic;
@@ -19,32 +21,101 @@ namespace Manufactures.Application.GarmentSubcon.InvoicePackingList.CommandHandl
         private readonly IStorage _storage;
         private readonly ISubconInvoicePackingListRepository _subconInvoicePackingListRepository;
         private readonly ISubconInvoicePackingListItemRepository _subconInvoicePackingListItemRepository;
+        private readonly ISubconInvoicePackingListReceiptItemRepository _subconInvoicePackingListReceiptItemRepository;
 
         public UpdateGarmentSubconInvoicePackingListCommandHandler(IStorage storage)
         {
             _storage = storage;
             _subconInvoicePackingListRepository = storage.GetRepository<ISubconInvoicePackingListRepository>();
             _subconInvoicePackingListItemRepository = storage.GetRepository<ISubconInvoicePackingListItemRepository>();
+            _subconInvoicePackingListReceiptItemRepository = storage.GetRepository<ISubconInvoicePackingListReceiptItemRepository>();
         }
         public async Task<SubconInvoicePackingList> Handle(UpdateGarmentSubconInvoicePackingListCommand request, CancellationToken cancellationToken)
         {
             var invoicePackingList = _subconInvoicePackingListRepository.Query.Where(o => o.Identity == request.Identity).Select(o => new SubconInvoicePackingList(o)).Single();
 
-            _subconInvoicePackingListItemRepository.Find(o => o.InvoicePackingListId == invoicePackingList.Identity).ForEach(async invoicePackingListItem =>
+            if(invoicePackingList.BCType == "BC 2.6.2" || invoicePackingList.BCType == "BC 2.7 IN")
             {
-                var item = request.Items.Where(o => o.Id == invoicePackingListItem.Identity).Single();
+                _subconInvoicePackingListReceiptItemRepository.Find(o => o.InvoicePackingListId == invoicePackingList.Identity).ForEach(async invoicePackingListReceiptItem =>
+                {
+                    var item = request.Items.Where(o => o.Id == invoicePackingListReceiptItem.Identity).SingleOrDefault();
+                    if (item == null)
+                    {
+                        invoicePackingListReceiptItem.Remove();
+                    }
 
-                invoicePackingListItem.SetDLNo(item.DLNo);
+                    await _subconInvoicePackingListReceiptItemRepository.Update(invoicePackingListReceiptItem);
+                });
 
-                invoicePackingListItem.Modify();
+                foreach(var item in request.Items)
+                {
+                    if (item.Id == Guid.Empty)
+                    {
+                        SubconInvoicePackingListReceiptItem subconInvoicePackingListReceiptItem = new SubconInvoicePackingListReceiptItem(
+                         Guid.NewGuid(),
+                         invoicePackingList.Identity,
+                         item.DLNo,
+                         item.DLDate,
+                         new ProductId(0),
+                         "-",
+                         "-",
+                         item.Product.Remark,
+                         item.Quantity,
+                         new UomId(item.Uom.Id),
+                         item.Uom.Unit,
+                         item.TotalPrice,
+                         item.PricePerDealUnit
+                       );
+                        await _subconInvoicePackingListReceiptItemRepository.Update(subconInvoicePackingListReceiptItem);
+                    }
+                }
+            }
+            else
+            {
+                _subconInvoicePackingListItemRepository.Find(o => o.InvoicePackingListId == invoicePackingList.Identity).ForEach(async invoicePackingListItem =>
+                {
+                    var item = request.Items.Where(o => o.Id == invoicePackingListItem.Identity).SingleOrDefault();
+                    if (item == null)
+                    {
+                        invoicePackingListItem.Remove();
 
-                await _subconInvoicePackingListItemRepository.Update(invoicePackingListItem);
-            });
+                        await _subconInvoicePackingListItemRepository.Update(invoicePackingListItem);
+                    }
+
+                    foreach (var itemm in request.Items)
+                    {
+                        if (item.Id == Guid.Empty)
+                        {
+                            SubconInvoicePackingListItem subconInvoicePackingListItem = new SubconInvoicePackingListItem(
+                            Guid.NewGuid(),
+                            invoicePackingList.Identity,
+                            itemm.DLNo,
+                            itemm.DLDate,
+                             new ProductId(0),
+                            "-",
+                            "-",
+                            "-",
+                            "-",
+                            itemm.Quantity,
+                            new UomId(0),
+                            "-",
+                            itemm.CIF,
+                            itemm.TotalPrice,
+                            itemm.TotalNW,
+                            itemm.TotalGW
+                        );
+
+                            await _subconInvoicePackingListItemRepository.Update(subconInvoicePackingListItem);
+                        }
+                    }
+                  
+                });
+            }
+            
 
             invoicePackingList.SetGW(request.GW);
             invoicePackingList.SetNW(request.NW);
             invoicePackingList.SetRemark(request.Remark);
-
 
             invoicePackingList.Modify();
 
---
+++ src/Manufactures.Data.EntityFrameworkCore/GarmentSubcon/InvoicePackingList/Configs/SubconInvoicePackingListReceiptItemConfig.cs
@@ -0,0 +1,37 @@
+﻿using Manufactures.Domain.GarmentSubcon.InvoicePackingList.ReadModels;
+using Manufactures.Domain.GarmentSubcon.InvoicePackingList.SubconInvoicePackingListReceiptItemReadModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Manufactures.Data.EntityFrameworkCore.GarmentSubcon.InvoicePackingList.ReceiptConfigs
+{
+    public class SubconInvoicePackingListReceiptItemConfig : IEntityTypeConfiguration<SubconInvoicePackingListReceiptItemReadModel>
+    {
+        public void Configure(EntityTypeBuilder<SubconInvoicePackingListReceiptItemReadModel> builder)
+        {
+            builder.ToTable("SubconInvoicePackingListReceiptItems");
+            builder.HasKey(e => e.Identity);
+            
+            builder.Property(p => p.DLNo).HasMaxLength(50);
+            builder.Property(p => p.ProductCode).HasMaxLength(25);
+            builder.Property(p => p.ProductName).HasMaxLength(100);
+            builder.Property(p => p.ProductRemark).HasMaxLength(255);
+
+            //builder.Property(p => p.DesignColor).HasMaxLength(255);
+            builder.Property(p => p.UomUnit).HasMaxLength(50);
+
+            builder.HasOne(w => w.SubconInvoicePacking)
+                .WithMany(m => m.SubconInvoicePackingListReceiptItem)
+                .HasForeignKey(s => s.InvoicePackingListId);
+
+            builder.ApplyAuditTrail();
+            builder.ApplySoftDelete();
+
+
+        }
+    }
+}
