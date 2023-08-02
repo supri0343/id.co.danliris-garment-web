60851ecf053b5ffc922d6198f8cf963188db0a31
AndreaZain <andrea.kinan@gmail.com>
2023-08-02 08:43:35 +0700

    create-receipt-subcon-cutting-out
    create-receipt-subcon-cutting-out

---
+++ src/WebApplication/Migrations/20230802013859_Create_Garment_Subcon_Cutting_Outs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DanLiris.Admin.Web.Migrations
+{
+    public partial class Create_Garment_Subcon_Cutting_Outs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
