ceb284c9c1b1388859f06018ac9b1843aae44f9b
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-08-21 11:00:36 +0700

    enhance-AddModul-SubconFinishingOut

--- src/WebApplication/Migrations/20230821034122_add-table-SubconFinishingOut.Designer.cs
+++ src/WebApplication/Migrations/20230821034122_add-table-SubconFinishingOut.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20230821034122_add-table-SubconFinishingOut")]
+    partial class addtableSubconFinishingOut
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
