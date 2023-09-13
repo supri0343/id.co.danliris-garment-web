6e43a42971758c5591bb3f5fb04becb156d69365
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-09-13 13:41:50 +0700

    enhance-AddModul-SubconPackingList

---
+++ src/Manufactures.Application/GermentReciptSubcon/GarmentPackingOuts/CommandHandlers/UpdateIsPackingListGarmentSubconPackinOutCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using ExtCore.Data.Abstractions;
+using Infrastructure.Domain.Commands;
+using Manufactures.Domain.GarmentPackingOut;
+using Manufactures.Domain.GarmentPackingOut.Commands;
+using Manufactures.Domain.GarmentPackingOut.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Manufactures.Application.GermentReciptSubcon.GarmentPackingIns.CommandHandlers
+{
+    public class UpdateIsPackingListGarmentSubconPackinOutCommandHandler : ICommandHandler<UpdateIsPackingListGarmentSubconPackingOutCommand, int>
+    {
+        private readonly IGarmentSubconPackingOutItemRepository _garmentPackingOutItemRepository;
+        private readonly IStorage _storage;
+
+        public UpdateIsPackingListGarmentSubconPackinOutCommandHandler(IStorage storage)
+        {
+            _garmentPackingOutItemRepository = storage.GetRepository<IGarmentSubconPackingOutItemRepository>();
+            _storage = storage;
+        }
+
+        public async Task<int> Handle(UpdateIsPackingListGarmentSubconPackingOutCommand request, CancellationToken cancellationToken)
+        {
+            List<Guid> guids = new List<Guid>();
+            foreach (var id in request.Identities)
+            {
+                guids.Add(Guid.Parse(id));
+            }
+            var Packings = _garmentPackingOutItemRepository.Query.Where(a => guids.Contains(a.Identity)).Select(a => new GarmentSubconPackingOutItem(a)).ToList();
+
+            foreach (var model in Packings)
+            {
+                model.SetIsPackingList(request.IsPackingList);
+                model.Modify();
+                await _garmentPackingOutItemRepository.Update(model);
+            }
+            _storage.Save();
+
+            return guids.Count();
+        }
+    }
+}
--- src/WebApplication/Migrations/20230907060835_add-Coloum-IsPackingList-SubconPackingOutItem.Designer.cs
+++ src/WebApplication/Migrations/20230907060835_add-Coloum-IsPackingList-SubconPackingOutItem.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20230907060835_add-Coloum-IsPackingList-SubconPackingOutItem")]
+    partial class addColoumIsPackingListSubconPackingOutItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -7735,7 +7737,7 @@ namespace DanLiris.Admin.Web.Migrations
                     b.Property<DateTimeOffset?>("DeletedDate");
 
                     b.Property<string>("DesignColor")
-                        .HasMaxLength(1000);
+                        .HasMaxLength(100);
 
                     b.Property<string>("ModifiedBy")
                         .HasMaxLength(32);
@@ -9091,35 +9093,6 @@ namespace DanLiris.Admin.Web.Migrations
                     b.ToTable("GarmentSubconCuttingRelations");
                 });
 
-            modelBuilder.Entity("Manufactures.Domain.LogHistories.ReadModels.LogHistoryReadModel", b =>
-
-            {
-                  b.Property<DateTimeOffset>("CreatedDate");
-                  b.Property<string>("CreatedBy")
-                   .IsRequired()
-                        .HasMaxLength(32);
-                    b.Property<bool?>("Deleted");
-
-                    b.Property<string>("DeletedBy");
-
-                      b.Property<string>("Activity")
-                      .HasMaxLength(1000);
-                    
-
-                    b.Property<DateTimeOffset?>("DeletedDate");
-
-                    b.Property<string>("Division")
-                        .HasMaxLength(255);
-
-                    b.Property<string>("ModifiedBy");
-
-                    b.Property<DateTimeOffset?>("ModifiedDate");
-
-                    b.HasKey("Identity");
-
-                    b.ToTable("LogHistory");
-
-                     });
             modelBuilder.Entity("Manufactures.Domain.GermentReciptSubcon.GarmentCuttingIns.ReadModels.GarmentSubconCuttingInDetailReadModel", b =>
                 {
                     b.Property<Guid>("Identity")
@@ -9136,7 +9109,6 @@ namespace DanLiris.Admin.Web.Migrations
 
                     b.Property<DateTimeOffset>("CreatedDate");
 
-    
                     b.Property<Guid>("CutInItemId");
 
                     b.Property<int>("CuttingInQuantity");
@@ -10360,13 +10332,13 @@ namespace DanLiris.Admin.Web.Migrations
                     b.Property<string>("BeacukaiType")
                         .HasMaxLength(20);
 
-                    b.Property<DateTimeOffset>("CreatedDate");
-
                     b.Property<string>("CreatedBy")
                         .IsRequired()
                         .HasMaxLength(32);
 
-                     b.Property<bool?>("Deleted");
+                    b.Property<DateTimeOffset>("CreatedDate");
+
+                    b.Property<bool?>("Deleted");
 
                     b.Property<string>("DeletedBy")
                         .HasMaxLength(32);
@@ -10404,7 +10376,6 @@ namespace DanLiris.Admin.Web.Migrations
                     b.Property<byte[]>("RowVersion")
                         .IsConcurrencyToken()
                         .ValueGeneratedOnAddOrUpdate();
-
 
                     b.Property<int>("UENItemId");
 
