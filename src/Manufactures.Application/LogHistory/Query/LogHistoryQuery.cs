fa64bae8d403ea9dc0f4b1179467aba1d3d26825
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-11-03 11:08:03 +0700

    Enhance-Add-Report-MonitoringLogHistoryDMS

---
+++ src/Manufactures.Application/LogHistory/Query/LogHistoryQuery.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Domain.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Application.LogHistories.Query
+{
+    public class LogHistoryQuery : IQuery<LogHistoryViewModel>
+    {
+        public DateTime dateFrom { get; private set; }
+        public DateTime dateTo { get; private set; }
+        public string token { get; private set; }
+
+        public LogHistoryQuery(DateTime dateFrom, DateTime dateTo,string token)
+        {
+            this.dateFrom = dateFrom;
+            this.dateTo = dateTo;
+            this.token = token;
+        }
+    }
+}
---
+++ src/WebApplication/Migrations/20231102070416_changeType-CreatedDate-LogHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DanLiris.Admin.Web.Migrations
+{
+    public partial class changeTypeCreatedDateLogHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "LogHistory",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "LogHistory",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+        }
+    }
+}
