c86cea5eb4cfc2185ee62a4e5f8518ab1df67be0
AndreaZain <andrea.kinan@gmail.com>
2023-11-29 15:04:32 +0700

    Enhance-Expenditure-Good-For-Omzet
    Enhance-Expenditure-Good-For-Omzet

---
+++ src/Manufactures.Application/GarmentExpenditureGoods/Queries/GetExpenditureGoodsForOmzet/GetExpenditureGoodsForOmzetQuery.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Domain.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Application.GarmentExpenditureGoods.Queries.GetExpenditureGoodsForOmzet
+{
+    public class GetExpenditureGoodsForOmzetQuery : IQuery<GarmentExpenditureGoodForOmzetListViewModel>
+    {
+        public string token { get; private set; }
+        public string unitcode { get; private set; }
+        public int offset { get; private set; }
+        public DateTime dateFrom { get; private set; }
+        public DateTime dateTo { get; private set; }
+
+        public GetExpenditureGoodsForOmzetQuery(DateTime dateFrom, DateTime dateTo, string unitcode, int offset, string token)
+        {
+            this.unitcode = unitcode;
+            this.offset = offset;
+            this.dateFrom = dateFrom;
+            this.dateTo = dateTo;
+            this.token = token;
+        }
+    }
+}
---
+++ src/Manufactures.Application/GarmentSample/SampleExpenditureGoods/Queries/GetSampleExpenditureGoodsForOmzet/GetSampleExpenditureGoodsForOmzetQuery.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Domain.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Application.GarmentExpenditureGoods.Queries.GetSampleExpenditureGoodsForOmzet
+{
+    public class GetSampleExpenditureGoodsForOmzetQuery : IQuery<GarmentSampleExpenditureGoodForOmzetListViewModel>
+    {
+        public string token { get; private set; }
+        public string unitcode { get; private set; }
+        public int offset { get; private set; }
+        public DateTime dateFrom { get; private set; }
+        public DateTime dateTo { get; private set; }
+
+        public GetSampleExpenditureGoodsForOmzetQuery(DateTime dateFrom, DateTime dateTo, string unitcode, int offset, string token)
+        {
+            this.unitcode = unitcode;
+            this.offset = offset;
+            this.dateFrom = dateFrom;
+            this.dateTo = dateTo;
+            this.token = token;
+        }
+    }
+}
